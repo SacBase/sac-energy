12b66fc009c9cd44248630e17a9634334848e0e9
Jordy Aaldering <aalderingjordy@gmail.com>
2025-03-25 16:34:41 +0100

    automatic build

--- src/src/rapl.c
+++ src/src/rapl.c
@@ -5,8 +5,6 @@
 #define MAX_PACKAGES 64
 #define RAPL_PATH "/sys/class/powercap/intel-rapl:%d/energy_uj"
 
-static int total_packages = MAX_PACKAGES;
-
 struct rapl
 {
     long long package_nanojoules[MAX_PACKAGES];
@@ -31,7 +29,7 @@ static long long read_energy(int package_id)
     return energy;
 }
 
-void read_energies(struct rapl **rapl)
+void rapl_init(struct rapl **rapl)
 {
     *rapl = (struct rapl *)malloc(sizeof(struct rapl));
 
@@ -46,20 +44,33 @@ void read_energies(struct rapl **rapl)
     (*rapl)->num_packages = package_id;
 }
 
-void update_energies(struct rapl *rapl)
+void rapl_reset(struct rapl *rapl)
 {
     long long energy;
-    for (int package_id = 0; package_id < rapl->num_packages; package_id += 1)
+    for (int package_id = 0; package_id < rapl->num_packages; package_id++)
     {
         energy = read_energy(package_id);
         assert(energy >= 0);
-        rapl->package_nanojoules[package_id] = energy - rapl->package_nanojoules[package_id];
+        rapl->package_nanojoules[package_id] = energy;
     }
 }
 
-void print_energies(struct rapl *rapl)
+void rapl_elapsed(long long **elapsed, struct rapl *rapl)
 {
-    for (int package_id = 0; package_id < rapl->num_packages; package_id += 1)
+    *elapsed = (long long *)malloc(sizeof(long long) * MAX_PACKAGES);
+
+    long long energy, diff;
+    for (int package_id = 0; package_id < rapl->num_packages; package_id++)
+    {
+        energy = read_energy(package_id);
+        assert(energy >= 0);
+        (*elapsed)[package_id] = energy - rapl->package_nanojoules[package_id];
+    }
+}
+
+void rapl_print(struct rapl *rapl)
+{
+    for (int package_id = 0; package_id < rapl->num_packages; package_id++)
     {
         printf("%lld,", rapl->package_nanojoules[package_id]);
     }
