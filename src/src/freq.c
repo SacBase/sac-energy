f0b4694ed715b95787613294331b7dfe6e4296f1
Jordy Aaldering <aalderingjordy@gmail.com>
2025-09-29 13:34:48 +0200

    fixes

--- src/src/freq.c
+++ src/src/freq.c
@@ -6,9 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <time.h>
 #include <stdint.h>
-#include <errno.h>
 
 #include "sac.h"
 #include "sacinterface.h"
@@ -16,13 +14,12 @@
 static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
                             int cpu, int group_fd, unsigned long flags)
 {
-    return syscall(__NR_perf_event_open, hw_event, pid,
-                   cpu, group_fd, flags);
+    return syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
 }
 
-static uint64_t read_counter(int fd)
+static unsigned long read_counter(int fd)
 {
-    uint64_t value = 0;
+    unsigned long value = 0;
     if (read(fd, &value, sizeof(value)) != sizeof(value)) {
         perror("read counter");
         return 0;
@@ -30,71 +27,53 @@ static uint64_t read_counter(int fd)
     return value;
 }
 
-void freqStart(int *fd_cycles_out, int *fd_ref_out)
+int cyclesStart(void)
 {
-    struct perf_event_attr pe_cycles, pe_ref;
-    memset(&pe_cycles, 0, sizeof(struct perf_event_attr));
-    memset(&pe_ref, 0, sizeof(struct perf_event_attr));
+    struct perf_event_attr pe;
+    memset(&pe, 0, sizeof(struct perf_event_attr));
 
-    // Count actual CPU cycles
-    pe_cycles.type = PERF_TYPE_HARDWARE;
-    pe_cycles.size = sizeof(struct perf_event_attr);
-    pe_cycles.config = PERF_COUNT_HW_CPU_CYCLES;
-    pe_cycles.disabled = 1;
-    pe_cycles.exclude_kernel = 0;
-    pe_cycles.exclude_hv = 0;
+    // Measure actual CPU cycles
+    pe.type = PERF_TYPE_HARDWARE;
+    pe.size = sizeof(struct perf_event_attr);
+    pe.config = PERF_COUNT_HW_CPU_CYCLES;
+    pe.disabled = 1;
+    pe.exclude_kernel = 0;
+    pe.exclude_hv = 0;
 
-    // Count reference (constant) cycles
-    pe_ref.type = PERF_TYPE_HARDWARE;
-    pe_ref.size = sizeof(struct perf_event_attr);
-    pe_ref.config = PERF_COUNT_HW_REF_CPU_CYCLES;
-    pe_ref.disabled = 1;
-    pe_ref.exclude_kernel = 0;
-    pe_ref.exclude_hv = 0;
-
-    int fd_cycles = perf_event_open(&pe_cycles, -1, 0, -1, 0);
-    if (fd_cycles == -1) {
-        perror("perf_event_open cycles");
+    int fd = perf_event_open(&pe, -1, 0, -1, 0);
+    if (fd == -1) {
+        perror("perf_event_open PERF_COUNT_HW_CPU_CYCLES");
         exit(1);
     }
 
-    int fd_ref = perf_event_open(&pe_ref, -1, 0, -1, 0);
-    if (fd_ref == -1) {
-        perror("perf_event_open ref");
+    if (ioctl(fd, PERF_EVENT_IOC_RESET, 0) < 0) {
+        perror("ioctl PERF_EVENT_IOC_RESET");
         exit(1);
     }
 
-    // Enable counters
-    ioctl(fd_cycles, PERF_EVENT_IOC_RESET, 0);
-    ioctl(fd_ref, PERF_EVENT_IOC_RESET, 0);
-    ioctl(fd_cycles, PERF_EVENT_IOC_ENABLE, 0);
-    ioctl(fd_ref, PERF_EVENT_IOC_ENABLE, 0);
+    if (ioctl(fd, PERF_EVENT_IOC_ENABLE, 0) < 0) {
+        perror("ioctl PERF_EVENT_IOC_ENABLE");
+        exit(1);
+    }
 
-    *fd_cycles_out = fd_cycles;
-    *fd_ref_out = fd_ref;
+    return fd;
 }
 
-double freqStop(int fd_cycles, int fd_ref)
+unsigned long cyclesStop(int fd)
 {
-    // Disable counters
-    ioctl(fd_cycles, PERF_EVENT_IOC_DISABLE, 0);
-    ioctl(fd_ref, PERF_EVENT_IOC_DISABLE, 0);
-
-    uint64_t cycles = read_counter(fd_cycles);
-    uint64_t ref = read_counter(fd_ref);
-
-    close(fd_cycles);
-    close(fd_ref);
-
-    if (ref > 0) {
-        double avg_freq = (double)cycles / (double)ref;
-        // Multiply by reference frequency (MHz)
-        avg_freq *= 100.0;
-        return avg_freq;
-    } else {
-        printf("Reference counter is zero!\n");
-        return 0.0;
+    if (ioctl(fd, PERF_EVENT_IOC_DISABLE, 0) < 0) {
+        perror("ioctl PERF_EVENT_IOC_DISABLE");
+        exit(1);
     }
+
+    unsigned long cycles = read_counter(fd);
+
+    if (close(fd) < 0) {
+        perror("close");
+        exit(1);
+    }
+
+    return cycles;
 }
 
 void *SAC_freq_create(void)
