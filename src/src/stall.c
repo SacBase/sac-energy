aeaa54ffda55d86666edbc1aa7a20b9d294d3779
Jordy Aaldering <aalderingjordy@gmail.com>
2025-10-06 13:53:26 +0200

    use bus cycles instead

--- src/src/stall.c
+++ src/src/stall.c
@@ -41,7 +41,7 @@ int stallStart(void)
 
     int fd = perf_event_open(&pe, 0, -1, -1, 0);
     if (fd == -1) {
-        perror("perf_event_open PERF_COUNT_HW_STALLED_CYCLES_FRONTEND");
+        perror("perf_event_open PERF_COUNT_HW_BUS_CYCLES");
         exit(1);
     }
 
