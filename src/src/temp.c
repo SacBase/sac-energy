535cb525d7522b47a6e20ef495e353a000c26979
Jordy Aaldering <aalderingjordy@gmail.com>
2025-10-20 12:01:09 +0200

    get temperature

---
+++ src/src/temp.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *SAC_temp_create(void)
+{
+    return (void*)0;
+}
+
+void SAC_temp_touch(void *obj)
+{
+    /* noop */
+}
+
+int SAC_temp_get(int zone)
+{
+    char path[128];
+    snprintf(path, sizeof(path), "/sys/class/thermal/thermal_zone%d/temp", zone);
+
+    FILE *fp = fopen(path, "r");
+    if (!fp) {
+        return 0;
+    }
+
+    long long temp;
+    if (fscanf(fp, "%d", &temp) <= 0) {
+        perror("fscanf");
+        return 0;
+    }
+
+    if (fclose(fp) < 0) {
+        perror("fclose");
+    }
+
+    return temp;
+}
